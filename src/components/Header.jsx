3fde8df12d32e4d6bea31794f67c5a20ec378e5e
Barny victor <victorbarny4@gmail.com>
2022-02-24 14:10:18 +0100

    worked on the navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import HeaderBody from "./HeaderBody/Headerbody";
+import Navbar from "./navbar/Navbar";
+import Connects from "./Pages/Connects";
+import Create from "./Pages/Create";
+import Learn from "./Pages/Learn";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFound from "./NotFound/NotFound";
+
+const HEADER = () => {
+  return (
+    <div className="Container">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HeaderBody />} />
+          <Route path="/learn" element={<Learn />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/connect" element={<Connects />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default HEADER;
---
+++ src/components/Pages/Create.jsx
@@ -0,0 +1,7 @@
+const Create = () => {
+  return (<div className="Create-Main_container">
+    nslnvlvnsdkjbvdfkvj
+  </div> );
+}
+ 
+export default Create;
